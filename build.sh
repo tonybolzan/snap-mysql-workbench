154de05db762e747ca636291cfdf375bf95721b9
Tonin Bolzan <tonin@bolzan.io>
2023-04-13 16:53:30 -0300

    Upgrade Ubuntu 22.04 (core22) and MySQL Workbench 8.0.32

--- build.sh
+++ build.sh
@@ -12,35 +12,42 @@ VERSION_ONLINE=$(curl -sS "http://repo.mysql.com/apt/ubuntu/dists/jammy/mysql-to
 
 VERSION_LOCAL=$(grep 'version' snapcraft.yaml |awk '{print $2}')
 
+function echo_red() {
+    echo -e "\033[0;31m${1}\033[0m"
+}
+
+function echo_green() {
+    echo -e "\033[0;32m${1}\033[0m"
+}
+
 if [ "$VERSION_LOCAL" != "$VERSION_ONLINE" ]; then
     echo >&2 "Version to be installed and local version don't match"
-    echo >&2 " - Online: $VERSION_ONLINE"
-    echo >&2 " - Local:  $VERSION_LOCAL"
+    echo_green >&2 " - Online: ${VERSION_ONLINE}"
+    echo_red   >&2 " - Local:  ${VERSION_LOCAL}"
     exit 1
 fi
 
-echo "Building Version:  $VERSION_LOCAL"
-
-# Unistall old snap
+echo_green "CleanUP installed versions"
 sudo snap remove mysql-workbench-community
 
-# Clean up old builds
+echo_green "CleanUP old builds"
 rm -f mysql-workbench*.snap
 
-# clean all cache
+echo_green "CleanUP cache"
 snapcraft clean
 
-# Build new image
-snapcraft --debug
+echo_green "Building Version: ${VERSION_LOCAL}"
+snapcraft
 
-# Install new image
+echo_green "Install Version: ${VERSION_LOCAL}"
 sudo snap install --dangerous mysql-workbench*.snap
+echo_green "Connect"
 sudo snap connect mysql-workbench-community:password-manager-service
 sudo snap connect mysql-workbench-community:ssh-keys
 sudo snap connect mysql-workbench-community:cups-control
 sudo snap connect mysql-workbench-community:removable-media
 
-# Run new snap
+echo_green "Run Version: ${VERSION_LOCAL}"
 snap run mysql-workbench-community --log-to-stderr
 
 # Deploy
